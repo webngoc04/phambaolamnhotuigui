3ee3370524b702a7279addd36e1ab921ad554803
webngoc04 <126089189+webngoc04@users.noreply.github.com>
2023-07-28 20:42:21 +0700

    Update 7.cpp

--- 7.cpp
+++ 7.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
 
+const int MOD = 1000000007;
+
+int main() {
+    int n;
+    cin >> n;
+
+    long long sum = 0;
+    for (int i = 1; i <= n; i++) {
+        long long cube = (long long)i * i * i;
+        sum = (sum + cube) % MOD;
+    }
+
+    cout<< sum << endl;
+
+    return 0;
